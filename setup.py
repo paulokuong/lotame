b016a006c2cb2b20f5255efc6a6c33c1c6dbeb2b
Paulo Kuong <nyds@ibm.com>
2019-04-02 16:54:41 -0400

    bump up version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as file:
     long_description = file.read()
 
 setup(name='lotame',
-      version='1.0.3',
+      version='1.0.4',
       description='LOTAME API Wrapper',
       install_requires=[
           'httplib2==0.10.3',
