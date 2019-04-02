51174276207c349ee38472d056532bb3d48f3605
Paulo Kuong <nyds@ibm.com>
2019-04-02 12:55:20 -0400

    changed version to 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as file:
     long_description = file.read()
 
 setup(name='lotame',
-      version='0.0.1',
+      version='1.0.0',
       description='LOTAME API Wrapper',
       install_requires=[
           'httplib2==0.10.3',
