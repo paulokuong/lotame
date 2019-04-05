920085b09ba453a5625dcf697fc128c8100e0511
Paulo Kuong <nyds@ibm.com>
2019-04-05 11:22:21 -0400

    change description

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ with open('README.rst') as file:
     long_description = file.read()
 
 setup(name='lotame',
-      version='1.0.4',
-      description='LOTAME API Wrapper',
+      version='1.0.4a',
+      description='Simple python wrapper for LOTAME API',
       install_requires=[
           'httplib2==0.10.3',
           'urllib3>=1.23',
@@ -18,7 +18,7 @@ setup(name='lotame',
           'Programming Language :: Python :: 3.6'
       ],
       keywords=(
-          'lotame api audience behavior api sdk'),
+          'lotame python wrapper client audience behavior api sdk'),
       url='https://github.com/paulokuong/lotame',
       author='Paulo Kuong',
       author_email='paulo.kuong@gmail.com',
